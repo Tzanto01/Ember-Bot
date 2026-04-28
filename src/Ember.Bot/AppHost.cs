1b30e5caea63af6f34aaa15a4b5b379a6dd7fdd1
Nico <nico.woellnitz@proton.me>
2026-04-28 14:10:07 +0200

    Allow CI integration hosts without appsettings

--- src/Ember.Bot/AppHost.cs
+++ src/Ember.Bot/AppHost.cs
@@ -28,7 +28,7 @@ public static class AppHost
         return Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration(config =>
             {
-                config.AddJsonFile("appsettings.json", optional: false)
+                config.AddJsonFile("appsettings.json", optional: !options.IncludeDiscordGateway)
                     .AddJsonFile("appsettings.Development.json", optional: true)
                     .AddEnvironmentVariables();
             })
